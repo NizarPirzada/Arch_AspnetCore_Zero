0dfd59b5ac15b005e0a3701deed96d20875e85d9
Nizar Adeel Abass <nizar.p@panacealogics.com>
2024-04-04 13:59:06 +0500

    first commit

---
+++ src/Arch.EntityFrameworkCore/EntityFrameworkCore/ArchDbContext.cs
@@ -0,0 +1,111 @@
+﻿using Abp.Zero.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Arch.Authorization.Delegation;
+using Arch.Authorization.Roles;
+using Arch.Authorization.Users;
+using Arch.Chat;
+using Arch.Editions;
+using Arch.Friendships;
+using Arch.MultiTenancy;
+using Arch.MultiTenancy.Accounting;
+using Arch.MultiTenancy.Payments;
+using Arch.OpenIddict.Applications;
+using Arch.OpenIddict.Authorizations;
+using Arch.OpenIddict.Scopes;
+using Arch.OpenIddict.Tokens;
+using Arch.Storage;
+
+namespace Arch.EntityFrameworkCore
+{
+    public class ArchDbContext : AbpZeroDbContext<Tenant, Role, User, ArchDbContext>, IOpenIddictDbContext
+    {
+        /* Define an IDbSet for each entity of the application */
+
+        public virtual DbSet<OpenIddictApplication> Applications { get; }
+        
+        public virtual DbSet<OpenIddictAuthorization> Authorizations { get; }
+        
+        public virtual DbSet<OpenIddictScope> Scopes { get; }
+        
+        public virtual DbSet<OpenIddictToken> Tokens { get; }
+        
+        public virtual DbSet<BinaryObject> BinaryObjects { get; set; }
+
+        public virtual DbSet<Friendship> Friendships { get; set; }
+
+        public virtual DbSet<ChatMessage> ChatMessages { get; set; }
+
+        public virtual DbSet<SubscribableEdition> SubscribableEditions { get; set; }
+
+        public virtual DbSet<SubscriptionPayment> SubscriptionPayments { get; set; }
+
+        public virtual DbSet<Invoice> Invoices { get; set; }
+
+
+        public virtual DbSet<SubscriptionPaymentExtensionData> SubscriptionPaymentExtensionDatas { get; set; }
+
+        public virtual DbSet<UserDelegation> UserDelegations { get; set; }
+        
+        public virtual DbSet<RecentPassword> RecentPasswords { get; set; }
+
+        public ArchDbContext(DbContextOptions<ArchDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BinaryObject>(b =>
+            {
+                b.HasIndex(e => new { e.TenantId });
+            });
+
+            modelBuilder.Entity<ChatMessage>(b =>
+            {
+                b.HasIndex(e => new { e.TenantId, e.UserId, e.ReadState });
+                b.HasIndex(e => new { e.TenantId, e.TargetUserId, e.ReadState });
+                b.HasIndex(e => new { e.TargetTenantId, e.TargetUserId, e.ReadState });
+                b.HasIndex(e => new { e.TargetTenantId, e.UserId, e.ReadState });
+            });
+
+            modelBuilder.Entity<Friendship>(b =>
+            {
+                b.HasIndex(e => new { e.TenantId, e.UserId });
+                b.HasIndex(e => new { e.TenantId, e.FriendUserId });
+                b.HasIndex(e => new { e.FriendTenantId, e.UserId });
+                b.HasIndex(e => new { e.FriendTenantId, e.FriendUserId });
+            });
+
+            modelBuilder.Entity<Tenant>(b =>
+            {
+                b.HasIndex(e => new { e.SubscriptionEndDateUtc });
+                b.HasIndex(e => new { e.CreationTime });
+            });
+
+            modelBuilder.Entity<SubscriptionPayment>(b =>
+            {
+                b.HasIndex(e => new { e.Status, e.CreationTime });
+                b.HasIndex(e => new { PaymentId = e.ExternalPaymentId, e.Gateway });
+            });
+
+            modelBuilder.Entity<SubscriptionPaymentExtensionData>(b =>
+            {
+                b.HasQueryFilter(m => !m.IsDeleted)
+                    .HasIndex(e => new { e.SubscriptionPaymentId, e.Key, e.IsDeleted })
+                    .IsUnique()
+                    .HasFilter("[IsDeleted] = 0");
+            });
+
+            modelBuilder.Entity<UserDelegation>(b =>
+            {
+                b.HasIndex(e => new { e.TenantId, e.SourceUserId });
+                b.HasIndex(e => new { e.TenantId, e.TargetUserId });
+            });
+            
+            modelBuilder.ConfigureOpenIddict();
+        }
+    }
+}
